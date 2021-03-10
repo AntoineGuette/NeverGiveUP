b7919450dcbb5c38db4ae64aaa1cf4e1ba474cf4
AntoineGuette <antoine.guette@epfedu.fr>
2021-03-10 16:10:11 +0100

    TEST

--- app/src/main/java/antoine/guette/nevergiveup/MainActivity.java
+++ app/src/main/java/antoine/guette/nevergiveup/MainActivity.java
@@ -10,6 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    ///Coucou
-    //Coucou le Guette
 }
