c28f4fad7aef488c42cfac7f242290cddf2f6aae
Rayane45 <rfougeroux01@gmail.com>
2021-04-07 10:54:29 +0200

    suite

--- app/src/main/java/antoine/guette/nevergiveup/TESTEXO.java
+++ app/src/main/java/antoine/guette/nevergiveup/TESTEXO.java
@@ -1,14 +1,29 @@
 package antoine.guette.nevergiveup;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class TESTEXO extends AppCompatActivity {
+
+    private TextView mTextOnEXOREP;
+    private Button mButtonRetour;
 
 
-public class TESTEXO extends Mouvements{
-    Mouvements Exo;
-
-    public TESTEXO(String nom, String description) {
-        super(nom, description);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exo_rep);
+        mTextOnEXOREP= findViewById(R.id.text_on_exorep); // association d'une variable au id du element que l'on veut
+        mButtonRetour = findViewById(R.id.button_retour);
     }
-    public void Burpees_10(Mouvements Burpees, int Nb_rep){
+        public void Burpees_10(Mouvements Burpees, int Nb_rep){
         Mouvements Burpees_10 = new Mouvements("Burpees","description");
         Nb_rep = 10;
         System.out.println("Burpees"+"Nb_rep");
